0ca195ff6da93f617947ad35956966c81d917487
Aadesh <aadeshmagare01@gmail.com>
2016-09-30 10:04:35 +0000

    completed

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require("express")
-
+var port = process.env.PORT || 8080;
 var app = express()
 
-app.get("/", function(req, res){
-    res.send("Hello man")
+app.get("/:timeString", function(req, res){
+    var par = req.params.timeString;
+    // console.log(par)
+    // console.log(Date.parse(par))
+    if(isNaN(Date.parse(par))){
+        var err = { "unix": null, "natural": null }
+        res.end(JSON.stringify(err));
+    }
+    else{
+        var date = new Date(par);
+        var nat = date.toLocaleString("en-us", { month: "long" }) + ' '+ date.getDate().toString() + ", " + date.getFullYear().toString()
+        console.log(nat)
+        var NatDate = {
+            unix:date.getTime(),
+            natural: nat
+        }
+        res.end(JSON.stringify(NatDate));
+    }
+
 })
-app.listen(process.env.PORT || 8080, function(){
-    console.log("listening")
+app.listen(port, function(){
+    console.log("listening on " + port)
 });
